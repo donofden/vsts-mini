6d2f9652d440ad2e229a27117c25909ee1d055b0
Aravind Kumar G <aravindkumar.ganesan@gmail.com>
2018-08-30 13:54:55 +0530

    Added Config Files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import { myConfig } from './config.js';
 
 class App extends Component {
+  constructor(){
+    super()
+      this.state = {
+        vstsToken: myConfig.vstsToken,
+        accountName: myConfig.accountName,
+        project: myConfig.project,
+        planID: myConfig.planID
+      }
+    }
   render() {
     return (
       <div>
@@ -14,7 +24,7 @@ class App extends Component {
             <hr></hr>
             <h3>Next Release</h3>
 
-            <h2 id="team" className="h2-style"></h2>
+            <h2 id="team" className="h2-style">{this.state.accountName}</h2>
             <p id="release" className="p-release-style"></p>
           </div>
       </div>
---
+++ src/config.sample.js
@@ -0,0 +1,6 @@
+export const myConfig = {
+    vstsToken: 'VSTS_TOKEN',
+    accountName: 'ACCOUNT_NAME',
+    project: 'PROJECT',
+    planID: 'PLAN_ID'
+};
