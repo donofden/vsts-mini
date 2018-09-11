29fee060d5b0ab2a85058140c558212d0fa4435b
Aravind Kumar G <aravindkumar.ganesan@gmail.com>
2018-09-11 19:50:21 +0530

    ADD: Home Screen

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,42 +1,31 @@
 import React, { Component } from 'react';
 import { myConfig } from '../../config.js';
+import './Home.css';
 
 class Home extends Component {
-    constructor(){
-        super()
-          this.state = {
-            planData: [],
-            iterationWorkItems: [],
-          };
-        this.getIterationData = this.getIterationData.bind(this);
-        
-        }
-    componentDidMount() {
-        let header = new Headers();
-        header.append("Authorization", "Basic " + myConfig.vstsToken);
-        this.setState({ workItemRelations: "" });
-        fetch("https://" + myConfig.accountName + ".visualstudio.com/" + myConfig.project + "/" + myConfig.teamId + "/_apis/work/teamsettings/iterations?api-version=4.1", {
-          method: "GET",
-          headers: header
-        }).then(response => response.json())
-          .then( planData => this.setState({planData: planData.value}))
-    }
-    getIterationData(event) {
-          let header = new Headers();
-          header.append("Authorization", "Basic " + myConfig.vstsToken);
-          
-          fetch("https://" + myConfig.accountName + ".visualstudio.com/" + myConfig.projectId + "/" + myConfig.teamId + "/_apis/work/teamsettings/iterations/" + event.target.value + "/workitems", {
-            method: "GET",
-            headers: header
-          }).then(response => response.json())
-            .then( iterationWorkItems => this.setState({iterationWorkItems: iterationWorkItems.workItemRelations}))
-    }
     render() {
       return (
         <div>
-            <h1 className="page-title">Home</h1>
+            <p className="title-txt">
+              <span data-text="V">V</span>
+              <span data-text="S">S</span>
+              <span data-text="T">T</span>
+              <span data-text="S">S</span>
+              <span data-text="-">-</span>
+              <span data-text="M">M</span>
+              <span data-text="I">I</span>
+              <span data-text="N">N</span>
+              <span data-text="I">I</span>
+            </p>
+            <div id='c'>
+              <div class='s'></div>
+              <div class='s'></div>
+              <div class='s'></div>
+              <div class='s'></div>
+              <div class='s'></div>
+              <div class='s'></div>
+            </div>
         </div>
-
       );
    }
 }
