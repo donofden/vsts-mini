6de2384e1e88d0e9ea25d0250d2c683a2e09f6aa
rsvijay2009 <rsvijay2009@gmail.com>
2018-12-12 09:50:43 +0530

    BUG: TypeError resolved
    https://trello.com/c/GULzyKKY

--- src/Components/Groups/Groups.js
+++ src/Components/Groups/Groups.js
@@ -9,7 +9,7 @@ class Groups extends Component {
             this.state = {
               teamsMembers: [],
               iterationList: [],
-              loading: false,
+              loading: true,
               iterationWorkItems: [],
               workItemDetails: [],
               emptyData: false,
@@ -29,44 +29,54 @@ class Groups extends Component {
           }).then(response => response.json())
             .then((teamsMembers) => {
                 this.setState({teamsMembers: teamsMembers.value})
+                if(this.state.teamsMembers.length > 0) {
+                    this.setState({ emptyData: true, loading: false})
+                }
+                else {
+                    this.setState({ emptyData: false, loading: false})
+                }
             })
       }
     render() {
+        let groupData;
+        if(this.state.emptyData && !this.state.loading) {
+            groupData = this.state.teamsMembers.map(member =>
+                <div className="col-xs-12 col-sm-6 col-md-3" key={member.identity['id']}>
+                <div className="image-flip" ontouchstart="this.classList.toggle('hover')">
+                    <div className="mainflip">
+                        <div className="frontside">
+                            <div className="card">
+                                <div className="card-body text-center">
+                                    <p><img className="img-fluid" src={member.identity['imageUrl']+"&size=2"} alt="Member Identity" /></p>
+                                    <h4 className="card-title">{member.identity['displayName']}</h4>
+                                    <p className="card-text">{member.identity['uniqueName']}</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="backside">
+                            <div className="card">
+                            <div className="card-body text-center">
+                                    <p><img className="img-fluid" src={member.identity['imageUrl']+"&size=2"} alt="Member Identity" /></p>
+                                    <h4 className="card-title">{member.identity['displayName']}</h4>
+                                    <p className="card-text">{member.identity['uniqueName']}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>)
+         } else if(!this.state.emptyData && !this.state.loading) {
+            groupData = <div className="text-center"><h3>No results found!!!</h3></div>
+         } else {
+            groupData = <div className="text-center"><img alt="Group members" src="../assets/img/build-load-icon.gif"></img></div>
+        }
       return (
          <div>
             <h2>Groups</h2>
             <section id="team" className="pb-5">
                 <div className="container">
                     <div className="row">
-                        {this.state.teamsMembers.map(member =>
-
-                        <div className="col-xs-12 col-sm-6 col-md-3" key={member.identity['id']}>
-                        <div className="image-flip" ontouchstart="this.classList.toggle('hover')">
-                            <div className="mainflip">
-                                <div className="frontside">
-                                    <div className="card">
-                                        <div className="card-body text-center">
-                                            <p><img className="img-fluid" src={member.identity['imageUrl']+"&size=2"} alt="Member Identity" /></p>
-                                            <h4 className="card-title">{member.identity['displayName']}</h4>
-                                            <p className="card-text">{member.identity['uniqueName']}</p>
-                                            {/* <a href="#" className="btn btn-primary btn-sm"><i className="fa fa-plus"></i></a> */}
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="backside">
-                                    <div className="card">
-                                    <div className="card-body text-center">
-                                            <p><img className="img-fluid" src={member.identity['imageUrl']+"&size=2"} alt="Member Identity" /></p>
-                                            <h4 className="card-title">{member.identity['displayName']}</h4>
-                                            <p className="card-text">{member.identity['uniqueName']}</p>
-                                            {/* <a href="#" className="btn btn-primary btn-sm"><i className="fa fa-plus"></i></a> */}
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                        )}
+                        {groupData}
                     </div>
                 </div>
             </section>
