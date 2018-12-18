6594b7db96d17520f262f0eb35eda1aed727b22e
rsvijay2009 <rsvijay2009@gmail.com>
2018-12-18 15:20:59 +0530

    Home page added for API End Point

--- python/app.py
+++ python/app.py
@@ -3,6 +3,10 @@ import json
 from TeamsModel import *
 from settings import *
 
+@app.route('/')
+def home_page():
+	return '<!DOCTYPE html><head><title>VSTS API END POINT</title></head ><body><div align="center" style="margin-top:60px; color:#5DBBF4"><h1> Welcome to VSTS API End Point</h1><img src="static/rest_api.gif" alt="rest_api"></body>'
+
 @app.route('/teams')
 def get_all_teams():
 	return jsonify({'teams': Team.get_all_teams()})
